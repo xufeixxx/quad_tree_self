d3f1a9ac430fa6f99a7f66ecc164ef12f4d0925f
xufei <xufei@example.com>
2021-04-15 13:15:45 +0800

    second modify

--- function/restructureQuadTree.py
+++ function/restructureQuadTree.py
@@ -1,6 +1,7 @@
 from overall.list import leaf_point_list, non_leaf_point_list
 from overall.setting import Setting
 import numpy as np
+
 setts = Setting()
 tree_level = setts.quad_tree_level
 
@@ -26,17 +27,39 @@ def tree_average_treatment():  # tp_value_1
         for i in range(num_of_point_top_h + 1, num_of_point_in_level + num_of_point_top_h + 1):
             point = non_leaf_point_list[i]
             if h == tree_level - 1:
-                sv1 = leaf_point_list[4 * i - 2].tp_value_1
-                sv2 = leaf_point_list[4 * i - 1].tp_value_1
-                sv3 = leaf_point_list[4 * i].tp_value_1
-                sv4 = leaf_point_list[4 * i + 1].tp_value_1
+                sv1, sv2, sv3, sv4 = from_f_id_get_s_point_v1(i, leaf_point_list)
             else:
-                sv1 = non_leaf_point_list[4 * i - 2].tp_value_1
-                sv2 = non_leaf_point_list[4 * i - 1].tp_value_1
-                sv3 = non_leaf_point_list[4 * i].tp_value_1
-                sv4 = non_leaf_point_list[4 * i + 1].tp_value_1
+                sv1, sv2, sv3, sv4 = from_f_id_get_s_point_v1(i, non_leaf_point_list)
             point.tp_value_1 = ((np.power(4, i) - np.power(4, i - 1)) / (np.power(4, i) - 1)) * point.tp_value \
                                + ((sv4 + sv1 + sv3 + sv2) * ((np.power(4, i - 1) - 1) / (np.power(4, i) - 1)))
         h -= 1
 
 
+def tree_mean_consistency():
+    h = 1
+    while h != tree_level:
+        num_of_point_top_h = int(1 / 3 * (np.power(4, h - 1) - 1))
+        num_of_point_in_level = np.power(4, h - 1)
+        if h == 1:
+            point = non_leaf_point_list[h]
+            sv1, sv2, sv3, sv4 = from_f_id_get_s_point_v1(h, non_leaf_point_list)
+            point.tp_value_2 = point.tp_value_1 + 1 / 4 * (-(sv1 + sv2 + sv3 + sv4))
+            h += 1
+        else:
+            for i in range(num_of_point_top_h + 1, num_of_point_in_level + num_of_point_top_h + 1):
+                point = non_leaf_point_list[i]
+                if h == tree_level - 1:
+                    sv1, sv2, sv3, sv4 = from_f_id_get_s_point_v1(i, leaf_point_list)
+                else:
+                    sv1, sv2, sv3, sv4 = from_f_id_get_s_point_v1(i, non_leaf_point_list)
+                point.tp_value_2 = point.tp_value_1 + \
+                                   1 / 4 * (non_leaf_point_list[point.father_id].tp_value_2 - (sv1 + sv2 + sv3 + sv4))
+            h += 1
+
+
+def from_f_id_get_s_point_v1(f_id, l_list):
+    sv1 = l_list[4 * f_id - 2].tp_value_1
+    sv2 = l_list[4 * f_id - 1].tp_value_1
+    sv3 = l_list[4 * f_id].tp_value_1
+    sv4 = l_list[4 * f_id + 1].tp_value_1
+    return sv1, sv2, sv3, sv4
--- main.py
+++ main.py
@@ -5,27 +5,25 @@ from function.makeQuadTree import make_quad_tree
 from overall.list import leaf_point_list, non_leaf_point_list
 from function.quadTreeDisturbance import read_dataSet, add_noise_list_and_h
 from function.quadTreeDisturbance import tp_value_set_one
-from function.restructureQuadTree import restructure_quad_tree, tree_average_treatment
+from function.restructureQuadTree import restructure_quad_tree, tree_average_treatment, tree_mean_consistency
 from overall.setting import Setting
 
 setts = Setting()
 
 
 def main():
-
-
-
     start_time = datetime.datetime.now()
 
     make_quad_tree(setts.x1, setts.x2, setts.y1, setts.y2)
 
-    kk = np.random.uniform([0, 0], [16, 16], (10, 2))
+    kk = np.random.uniform([0, 0], [16, 16], (100, 2))
 
-    for i in range(10):
+    for i in range(100):
         id_list = tp_value_set_one(kk[i][0], kk[i][1])
         oz, h = add_noise_list_and_h(id_list)
         restructure_quad_tree(oz, h)
     tree_average_treatment()
+    tree_mean_consistency()
     print("HelloWorld!!")
 
     print("用时：", datetime.datetime.now() - start_time)
